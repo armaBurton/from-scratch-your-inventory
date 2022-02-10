996f82e9c39519fb692223515df4d95f5664274e
armaBurton <armanarma@armaburton.com>
2022-02-10 14:40:59 -0800

    signIn/SignUp functionality works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import AuthPage from './AuthPage/AuthPage';
 
 function App() {
-  return <></>;
+  const [user, setUser] = useState('');
+
+  return <Router>
+    <div className="App">
+      {
+        !user
+          ? <header></header>
+          : <header>
+            <Link to='#'>Dumb Link</Link>
+            <Link to='#'>Dummy Link</Link>
+            <Link to='#'>Dumber Link</Link>
+          </header>
+      }
+      <main>
+        <Switch>
+          <Route exact path='/'>
+            {
+              user
+                ? <Redirect to='/inventory' />
+                : <AuthPage setUser={setUser} />
+            }
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  </Router>;
 }
 
 export default App;
---
+++ src/AuthPage/AuthPage.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { signIn, signUp } from '../services/fetch-utils';
+
+export default function AuthPage({ setUser }){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignIn(e){
+    e.preventDefault();
+    const user = await signIn(email, password);
+    setUser(user);
+  }
+  
+  async function handleSignUp(e){
+    e.preventDefault();
+    const user = await signUp(email, password);
+    setUser(user);
+  }
+
+  return <div className='auth'>
+    <h2><em>Inventorizo</em></h2>
+
+    <form onSubmit={handleSignIn}>
+      <label>
+        Email
+        <input required value={email} type='email' name='email' onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password
+        <input required value={password} type='password' name='password' onChange={e => setPassword(e.target.value)}/>
+      </label>
+      <div className="button-div">
+        <button onClick={handleSignIn}>Sign In</button>
+        <button onClick={handleSignUp}>Sign Up</button>
+      </div>
+    </form>
+  </div>; 
+}
